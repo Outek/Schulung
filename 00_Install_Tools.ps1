dac1332a9aee10fed3a9487f5869df79669357a6
Outek <daniel.siegenthaler.1@gmail.com>
2017-06-12 14:44:03 +0200

    Korrektur

--- 00_Install_Tools.ps1
+++ 00_Install_Tools.ps1
@@ -2,11 +2,27 @@
 #Go to www.github.com
 #Search for "outek" , switch to users
 
-#download zip file
+#download zip file, save and unzip it somewhere..
 https://github.com/Outek/Schulung
+
+#Set ExecutionPolicy
+Set-ExecutionPolicy Unrestricted
 
 #install chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+#close powershell, open new session as admin
+
 #install vscode, vscode_powershell extension for vscode, chrome, git
 choco install visualstudiocode git googlechrome vscode-powershell -y
+
+#open visual studio code
+#activate powershell extension, click on reload to activate the extension
+
+#activate integtrated terminal
+# go to "view", click on integrated terminal
+
+#click on customize, chose powershell.exe
+#git clone https://github.com/Outek/Schulung.git
+#click on "open folder", choose path of the downloaded and unzipped folder from git
+#C:\Temp\Schulung

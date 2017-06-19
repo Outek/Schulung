cf40f56eeb6c9b84bd234fe9cbae30a730bfb58e
Outek <daniel.siegenthaler.1@gmail.com>
2017-06-19 18:18:10 +0200

    Test

--- Working/05_Git/00_git.ps1
+++ Working/05_Git/00_git.ps1
@@ -1,10 +1,12 @@
 #Username: PFTestuser01
 #Passwort: Beispiel_Passwort
 
-git config --global user.email ""
-git config --global user.name ""
+git config --global user.email "email"
+git config user.email "email"
+
+git config --global user.name "username"
+git config user.name "username"
 
 #Git einrichten
-
 
 #Git ignore file
